cf9ee88edf9097bab418fa5b353cddc8a5d5bdeb
Vallium <anton.alliot@gmail.com>
2017-10-05 21:38:16 +0200

    [+ Chmapion] Adding Champion class

---
+++ champion/champion.go
@@ -0,0 +1,40 @@
+package champion
+
+import (
+	inst "github.com/Vallium/corewar-champion-g-go/instruction"
+)
+
+type Champion struct {
+	name 			string
+	comment 		string
+	instructions	[]inst.Instruction
+}
+
+func (c *Champion) SetName(name string) {
+	c.name = name
+}
+
+func (c *Champion) SetComment(comment string) {
+	c.comment = comment
+}
+
+func (c *Champion) GetName() (string) {
+	return c.name
+}
+
+func (c *Champion) GetComment() (string) {
+	return c.comment
+}
+
+func (c Champion) PushInstruction(instruction string) {
+	i := inst.Create(instruction)
+	c.instructions = append(c.instructions, i)
+}
+
+func Create(name string, comment string) (*Champion) {
+	var c Champion
+
+	c.SetName(name)
+	c.SetComment(comment)
+	return &c
+}
