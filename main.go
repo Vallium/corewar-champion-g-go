18d28d868444d133f2745b3e0adaf2d0617856ce
Vallium <anton.alliot@gmail.com>
2017-10-05 21:37:15 +0200

    [~ Parser] Parsing nom name, description and instructions

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 
-	// parser "github.com/Vallium/corewar-champion-g-go/parser"
+	parser "github.com/Vallium/corewar-champion-g-go/parser"
 )
 
 func main() {
 	fmt.Println("Corewar champion G");
+	parser.Parse("./winners-2014/_-clear.s")
 }
--- parser/parser.go
+++ parser/parser.go
@@ -2,22 +2,40 @@ package parser
 
 import (
 	"fmt"
+	"strings"
 	"bufio"
 	"os"
+
+	champ "github.com/Vallium/corewar-champion-g-go/champion"
 )
 
-func Parse(path string) ([]string, error) {
+func Parse(path string) (*champ.Champion, error) {
+	var champion *champ.Champion
 	file, err := os.Open(path)
+
+	champion = champ.Create("", "")
+
 	if err != nil {
-		return nil, err
+		return champion, err
 	}
 	defer file.Close()
 
 	var lines []string
 	scanner := bufio.NewScanner(file)
+
+	index := 0
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		if (index == 0) {
+			champion.SetName(strings.Split(scanner.Text(), "\"")[1])
+		} else if (index == 1) {
+			champion.SetComment(strings.Split(scanner.Text(), "\"")[1])
+		} else {
+			
+		}
 		lines = append(lines, scanner.Text())
+		index++
 	}
-	return lines, scanner.Err()
-} 
+	fmt.Println(champion.GetName())
+	fmt.Println(champion.GetComment())
+	return champion, scanner.Err()
+}
