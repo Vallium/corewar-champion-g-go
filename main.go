779ede7aebd69e8bc48f2519a7adb5a96c2332bb
Vallium <anton.alliot@gmail.com>
2017-10-05 17:43:31 +0200

    [+] Add a parser as a packe local

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	// parser "github.com/Vallium/corewar-champion-g-go/parser"
+)
+
+func main() {
+	fmt.Println("Corewar champion G");
+}
---
+++ parser/parser.go
@@ -0,0 +1,23 @@
+package parser
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func Parse(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+} 
