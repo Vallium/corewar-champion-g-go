620fe857fa2ab3b1ea12d31b5d3c19f6eea54e15
Vallium <anton.alliot@gmail.com>
2017-12-14 19:18:49 +0100

    feat(Champions): Add winners of CW championship 2017

---
+++ winners-2014/Explosive_Kitty.s
@@ -0,0 +1,121 @@
+.name "Explosive Kitty"
+.comment "Jan-ken-pon (じゃんけんぽん)"
+
+ld -42, r2
+zjmp %73
+sti r10, r15, r9
+live %42
+zjmp %-410
+ld %50986512, r10
+ld %-392, r9
+ld %84540929, r2
+ld %-487, r3
+ld %118095856, r4
+ld %-502, r5
+ld %-7, r7
+ld -511, r1
+zjmp %-67
+ld %55575553, r8
+st r1, r15
+st r8, -4
+fork %298
+live %42
+fork %-81
+ld %190054913, r10
+ld %-396, r9
+ld %190055425, r2
+ld %-491, r3
+st r2, r4
+ld %-506, r5
+ld %-11, r7
+ld -511, r1
+zjmp %-148
+st r4, -37
+st r4, -47
+st r4, -57
+st r4, -67
+st r4, -77
+st r4, -87
+st r4, -97
+st r4, -107
+st r4, -117
+st r4, -127
+st r4, -137
+st r4, -147
+st r4, -157
+st r4, -167
+st r4, -177
+st r4, -187
+st r4, -197
+st r4, -207
+st r4, -217
+st r4, -227
+st r4, -237
+st r4, -247
+st r4, -257
+st r4, -267
+st r4, -277
+st r4, -287
+st r4, -297
+st r4, -307
+st r4, -317
+st r4, -327
+st r4, -337
+st r4, -347
+st r4, -357
+st r4, -367
+st r4, -377
+st r4, -387
+st r4, -397
+st r4, -407
+st r4, -417
+st r4, -427
+st r4, -437
+st r4, -447
+st r4, -457
+st r4, -467
+st r4, -477
+live %42
+zjmp %-230
+ld %252645135, r4
+live %42
+ld 511, r1
+fork %16
+st r15, -27
+live %42
+zjmp %-263
+live %42
+fork %-271
+live %42
+fork %47
+live %42
+fork %91
+ld %5, r3
+ld %511, r5
+ld %190055429, r2
+ld %33554431, r6
+ld 511, r1
+zjmp %118
+live %42
+fork %80
+ld %9, r3
+ld %506, r5
+ld %17519622, r2
+ld 17, r6
+ld 511, r1
+zjmp %76
+live %42
+sti r2, r3, r1
+ld %13, r3
+ld %501, r5
+ld %50401039, r2
+st r2, r6
+ld 511, r1
+zjmp %33
+st r15, -42
+ld %511, r3
+ld %496, r5
+st r4, r2
+st r4, r6
+zjmp %3
+sti r2, r3, r1
---
+++ winners-2014/gateau.s
@@ -0,0 +1,142 @@
+.name "🎂"
+.comment "🎂"
+
+ld %0, r10
+zjmp %356
+live %2
+ld %251658240, r2
+ld %0, r10
+fork %46
+st r2, -511
+st r2, -505
+st r2, -499
+st r2, -493
+st r2, -487
+st r2, -481
+st r2, -475
+st r2, -469
+zjmp %-40
+live %2
+st r2, -471
+st r2, -465
+st r2, -459
+st r2, -453
+st r2, -447
+st r2, -441
+st r2, -435
+zjmp %-35
+ld %71, r1
+aff r1
+ld %82, r1
+aff r1
+ld %79, r1
+aff r1
+ld %83, r1
+aff r1
+ld %32, r1
+aff r1
+ld %83, r1
+aff r1
+ld %65, r1
+aff r1
+ld %67, r1
+aff r1
+ld %32, r1
+aff r1
+ld %71, r1
+aff r1
+ld %82, r1
+aff r1
+ld %79, r1
+aff r1
+ld %83, r1
+aff r1
+ld %32, r1
+aff r1
+ld %83, r1
+aff r1
+ld %65, r1
+aff r1
+ld %67, r1
+aff r1
+ld %32, r1
+aff r1
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+lfork %0
+live %2
+st r1, -363
+st r16, -5
+st r1, 143
+st r16, -5
+fork %-373
+live %2
+fork %154
+live %2
+fork %-5
+live %2
+fork %11
+or r2, %32, r2
+live %2
+fork %11
+or r2, %16, r2
+live %2
+fork %11
+or r2, %8, r2
+live %2
+fork %11
+or r2, %4, r2
+ldi %13, r2, r16
+ld %0, r15
+sti r16, %64, r2
+sti r16, %122, r2
+sti r16, %180, r2
+sti r16, %238, r2
+sti r16, %296, r2
+sti r16, %354, r2
+live %2
+live %2
+sti r16, %402, r2
+sti r16, %448, r2
+zjmp %442
+live %2
+live %2
+ld %507, r6
+ld %8, r8
+ld %139, r10
+fork %53
+live %2
+ld %4, r2
+ld %101253622, r3
+ld %101253622, r5
+ld %168362119, r7
+ld %262144, r9
+ld %0, r16
+zjmp %53
+live %2
+ld %0, r2
+ld %190055682, r3
+ld %190056194, r5
+ld %190056706, r7
+ld %134443017, r9
+ld %0, r16
+zjmp %3
+sti r3, r2, r6
+zjmp %502
