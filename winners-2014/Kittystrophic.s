8233fcdb09eb72d96fb923c2914ab773c65de500
Vallium <anton.alliot@gmail.com>
2017-12-15 17:31:56 +0100

    feat(Champions): Add winners of CW championship 2017

--- winners-2014/Kittystrophic.s
+++ winners-2014/Kittystrophic.s
@@ -1,11 +1,5 @@
 .name "Kittystrophic"
-.comment "Jan-ken-pon (じゃんけんぽん)
-Ce qui reste éternellement incompréhensible dans la nature, c’est qu’on puisse la comprendre.
-Celui qui ne peut plus éprouver ni étonnement ni surprise, est pour ainsi dire mort : ses yeux sont éteints. – Albert Einstein
-Si vous voulez trouver les secrets de l'univers pensez en termes de fréquences, d'énergies et de vibrations. - Nikola Tesla
-Pas la pour etre ici. - Silicon Val[list]
-Dieu est la plus belle invention.
-Tapis dans l'ombre je veille. - Minou"
+.comment "Jan-ken-pon (じゃんけんぽん)"
 
 fork %235
 live %252645135
